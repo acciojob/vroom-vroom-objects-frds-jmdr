565daa33e976bf125cdf03410bdc5c7da185ebe3
Firdosh Jamadar <firdoshjamadar0@gmail.com>
2025-02-11 10:53:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 // Complete the js code
-function Car(make, model) {}
+// function Car(make, model) {}
 
-function SportsCar(make, model, topSpeed) {}
+// function SportsCar(make, model, topSpeed) {}
+
+	class Car{
+		constroctor(make, model){
+			this.make= make;
+			this.model= model;
+		}
+	}
+Car.prototype.getMakeModel() = function(){
+	return`${this.make} ${this.model}`;
+}
+class SportsCar extends Car{
+	constructor(make, model, topSpeed) {
+		super(make, model);
+		this.topSpeed=topSpeed;
+		
+	}
+}
+SportsCar.prototype.getTopSpeed()= function() {
+	return this.topSpeed;
+}
 
 // Do not change the code below
 window.Car = Car;
